e318cca4bdd61e9fbc349383766fd0cda0b05d71
samar12-rad <v.samarth1212@gmail.com>
2025-02-18 18:26:22 +0530

    Doing first workflow run check 11

--- cypress/e2e/testspec.cy.js
+++
@@ -1,9 +0,0 @@
-
-	describe('template spec', () => {
-		it('passes', () => {
-		  cy.visit('https://example.cypress.io')
-		})
-		it('passes on google', () => {
-			cy.visit('https://google.com')
-		  })
-	  });
